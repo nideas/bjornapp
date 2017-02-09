34a4667ad046741e9e1cd25d363db9e4f0654ce4
Per Karlsson <per@pkarlsson.com>
2017-02-09 16:47:14 +0000

    updated texts

--- common/components/App.js
+++ common/components/App.js
@@ -5,12 +5,12 @@ import { StyleSheet, css } from 'aphrodite'
 
 const App = ({ children }) => (
   <div className={css(styles.root)}>
-    <Helmet title='React Production Starter' titleTemplate='%s - React Production Starter' />
-    <h1 className={css(styles.title)}>React Production Starter</h1>
+    <Helmet title='Bjorn' titleTemplate='%s - Bjorn' />
+    <h1 className={css(styles.title)}>Bjorn</h1>
     <Nav />
     {children}
     <footer className={css(styles.footer)}>
-      Copyright © 2016 <a className={css(styles.footerLink)} href='http://twitter.com/jaredpalmer' target='_blank'>Jared Palmer</a>
+      
     </footer>
   </div>
 )
